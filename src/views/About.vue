9a1801ba0074a4dc451fcfa3472e3536f73ce532
wzz <2804355025@qq.com>
2020-10-03 11:35:02 +0800

    1.0.1

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,8 +3,8 @@
     <Title active-index="4"></Title>
     <H1>this is the about page</H1>
     <h2>学生信息管理系统</h2>
-    <h2>https://github.com/wangzz-yyzz/StudentManager-Vue-Front.git</h2>
-    <h2>https://github.com/wangzz-yyzz/StudentManager-Vue-Rear.git</h2>
+    <p>https://github.com/wangzz-yyzz/StudentManager-Vue-Front.git</p>
+    <p>https://github.com/wangzz-yyzz/StudentManager-Vue-Rear.git</p>
     <h3>wzz</h3>
   </div>
 </template>
